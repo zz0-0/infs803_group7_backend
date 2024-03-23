3ebf095d006e0d3e82233edf9b5473b52ebed0f9
zz0-0 <122845086+zz0-0@users.noreply.github.com>
2024-03-23 17:12:16 +1300

    add firebase support

--- src/user.rs
+++ src/user.rs
@@ -1,15 +1,16 @@
-use std::{fs::File, io::Read};
+use std::{collections::HashMap, fs::File, io::Read};
 
 use axum::{
-    body::Body, http::StatusCode, response::{IntoResponse, Response}, Json
+    extract::Path, http::StatusCode, response::IntoResponse, Json
     
 };
 use serde::{Deserialize, Serialize};
 use chrono::{DateTime, Utc};
 use chrono::serde::ts_seconds_option;
 use serde_json::{json, Value};
+use firebase_rs::Firebase;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct User {
     pub id: Option<i32>,
     pub name: String,
@@ -20,44 +21,41 @@ pub struct User {
     pub updated_at: Option<DateTime<Utc>>
 }
 
-#[derive(Serialize, Deserialize)]
-pub struct UserResponse {
-    pub result: User,
-}
-
-#[derive(Serialize, Deserialize)]
-pub struct UserListResponse {
-    pub result: Vec<User>,
-}
-
-// #[derive(Debug)]
-// pub struct AppError {
-//     code: StatusCode,
-//     message: String,
-// }
-
 pub  async fn fetch_users() -> (StatusCode, Json<Value>) {
-    // let users = vec![];
+    let firebase =  Firebase::new("").unwrap();
+    let firebase = firebase.at("users");
+    let users = firebase.get::<HashMap<String, User>>().await;
+
     let mut file = File::open("src/example.json").unwrap();
     let mut data = String::new();
     file.read_to_string(&mut data).unwrap();
-    // let json = serde_json::from_str(&data);
 
     let mut json_result: Vec<User> = Vec::new();
     json_result.push(User { id: Some(1), name: "a".to_owned(),level:Some(1),  created_at: Some(Utc::now()), updated_at: Some(Utc::now()) });
     
-    // Ok(Json(json!(UserListResponse{result : json_result})))
     (StatusCode::OK,Json(json!(json_result)))
-    // Json(str)
 }
 
-pub async fn create_user() -> impl IntoResponse {
-    Response::builder().status(StatusCode::CREATED).body(Body::from("User created successfully"))
-    .unwrap()
+pub async fn create_user(Json(user): Json<User>) -> impl IntoResponse {
+    let firebase =  Firebase::new("").unwrap();
+    let firebase = firebase.at("users");
+    let _users = firebase.set::<User>(&user).await;
 }
 
-pub async fn fetch_user() -> impl IntoResponse {}
+pub async fn fetch_user(Path(id): Path<String>) -> impl IntoResponse {
+    let firebase =  Firebase::new("").unwrap();
+    let firebase = firebase.at("users").at(&id);
+    let user = firebase.get::<User>().await;
+}
 
-pub async fn update_user() -> impl IntoResponse {}
+pub async fn update_user(Json(user): Json<User>) -> impl IntoResponse {
+    let firebase =  Firebase::new("").unwrap();
+    let firebase = firebase.at("users").at(&user.id.unwrap().to_string());
+    let _user = firebase.update::<User>(&user).await;
+}
 
-pub async fn delete_user() -> impl IntoResponse {}
+pub async fn delete_user(Path(id): Path<String>) -> impl IntoResponse {
+    let firebase =  Firebase::new("").unwrap();
+    let firebase = firebase.at("users").at(&id);
+    let _result = firebase.delete().await;
+}
