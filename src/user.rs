5b6fdb957ec759b0443126cb4e8299cce9097219
zz0-0 <122845086+zz0-0@users.noreply.github.com>
2024-03-25 18:32:15 +1300

    change return value to standard type

--- src/user.rs
+++ src/user.rs
@@ -1,9 +1,4 @@
-use std::{collections::HashMap, fs::File, io::Read};
-
-use axum::response::Response;
 use axum::{extract::Path, http::StatusCode, response::IntoResponse, Json};
-use chrono::serde::ts_seconds_option;
-use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
 use firebase_rs::Firebase;
@@ -22,8 +17,8 @@ pub struct User {
 
 pub async fn fetch_users() -> Result<impl IntoResponse, (StatusCode, Json<Value>)> {
     let firebase = Firebase::new("https://infs803-group7-default-rtdb.firebaseio.com/").unwrap();
-    let firebase = firebase.at("users");
-    let users = firebase.get::<Vec<User>>().await.map_err(|e| {
+    let data = firebase.at("users");
+    let users = data.get::<Vec<User>>().await.map_err(|e| {
         let error = serde_json::json!({"message": format!("Database error: { }", e)});
         (StatusCode::INTERNAL_SERVER_ERROR, Json(error))
     });
@@ -31,26 +26,73 @@ pub async fn fetch_users() -> Result<impl IntoResponse, (StatusCode, Json<Value>
     Ok((StatusCode::OK, Json(json_response)))
 }
 
-pub async fn create_user(Json(user): Json<User>) -> impl IntoResponse {
-    let firebase = Firebase::new("").unwrap();
-    let firebase = firebase.at("users");
-    let _users = firebase.set::<User>(&user).await;
+pub async fn create_user(
+    Json(user): Json<User>,
+) -> Result<impl IntoResponse, (StatusCode, Json<Value>)> {
+    let firebase = Firebase::new("https://infs803-group7-default-rtdb.firebaseio.com/").unwrap();
+    let data = firebase.at("users");
+    let users = data.set::<User>(&user).await;
+    match users {
+        Ok(_) => Ok((
+            StatusCode::OK,
+            Json(serde_json::json!({"message": "create user successful"})),
+        )),
+        Err(e) => Ok((
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(serde_json::json!({"message": format!("create user fail: { }", e)})),
+        )),
+    }
 }
 
-pub async fn fetch_user(Path(id): Path<String>) -> impl IntoResponse {
-    let firebase = Firebase::new("").unwrap();
-    let firebase = firebase.at("users").at(&id);
-    let user = firebase.get::<User>().await;
+pub async fn fetch_user(
+    Path(id): Path<String>,
+) -> Result<impl IntoResponse, (StatusCode, Json<Value>)> {
+    let firebase = Firebase::new("https://infs803-group7-default-rtdb.firebaseio.com/").unwrap();
+    let data = firebase.at("users").at(&id);
+    let user = data.get::<User>().await;
+    let json_response = serde_json::json!(user.as_ref().unwrap());
+    match user.as_ref() {
+        Ok(_) => Ok((StatusCode::OK, Json(serde_json::json!(json_response)))),
+        Err(e) => Ok((
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(serde_json::json!({"message": format!("create user fail: { }", e)})),
+        )),
+    }
 }
 
-pub async fn update_user(Json(user): Json<User>) -> impl IntoResponse {
-    let firebase = Firebase::new("").unwrap();
-    let firebase = firebase.at("users").at(&user.id.unwrap().to_string());
-    let _user = firebase.update::<User>(&user).await;
+pub async fn update_user(
+    Path(id): Path<String>,
+    Json(user): Json<User>,
+) -> Result<impl IntoResponse, (StatusCode, Json<Value>)> {
+    let firebase = Firebase::new("https://infs803-group7-default-rtdb.firebaseio.com/").unwrap();
+    let data = firebase.at("users").at(&id);
+    let user = data.update::<User>(&user).await;
+    match user {
+        Ok(_) => Ok((
+            StatusCode::OK,
+            Json(serde_json::json!({"message": "update user successful"})),
+        )),
+        Err(e) => Ok((
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(serde_json::json!({"message": format!("update user fail: { }", e)})),
+        )),
+    }
 }
 
-pub async fn delete_user(Path(id): Path<String>) -> impl IntoResponse {
-    let firebase = Firebase::new("").unwrap();
-    let firebase = firebase.at("users").at(&id);
-    let _result = firebase.delete().await;
+pub async fn delete_user(
+    Path(id): Path<String>,
+) -> Result<impl IntoResponse, (StatusCode, Json<Value>)> {
+    let firebase = Firebase::new("https://infs803-group7-default-rtdb.firebaseio.com/").unwrap();
+    let data = firebase.at("users").at(&id);
+    let user = data.delete().await;
+    match user {
+        Ok(_) => Ok((
+            StatusCode::OK,
+            Json(serde_json::json!({"message": "delete user successful"})),
+        )),
+        Err(e) => Ok((
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(serde_json::json!({"message": format!("delte user fail: { }", e)})),
+        )),
+    }
 }
